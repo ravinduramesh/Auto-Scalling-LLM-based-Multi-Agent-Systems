7f6e57e35f3befa2425318adaa230a3428b21543
Ravindu Perera <ravinduramesh@gmail.com>
2024-04-01 12:26:13 +0530

    Rewamp autogen code to get better responses and make it simmilar to the noval approach.

--- Existing Solution/autogenTest.py
+++ Existing Solution/autogenTest.py
@@ -1,54 +1,75 @@
 import autogen
+import json
+
 
 llm_config = {
-    "config_list": [{"model": "gpt-3.5-turbo-0125", "api_key": "YOUR_OPEN_API_KEY"}],
+    "config_list": [{"model": "gpt-4-turbo-preview", "api_key": "api-key"}],
 }
 
-user_proxy = autogen.UserProxyAgent(
-    name="User_proxy",
-    system_message="A patient that wanted a treatment for his illness.",
+patient = autogen.UserProxyAgent(
+    name="Patient",
     human_input_mode="TERMINATE",
+    code_execution_config=False,
 )
+
+agentSystemPromptPrefix = " Give your ideas to add more information and insight as a single dialog. Make your dialog shorter and to the point."
 
 generalDoctor = autogen.AssistantAgent(
     name="GeneralDoctor",
-    system_message="A general doctor that can diagnose and treat common illnesses.",
+    system_message="You are a general doctor at the OPD ward who provides medical advice, and guidance on common health concerns. You have to offer reassurance and recommend further evaluation or consultation with specialists if necessary." + agentSystemPromptPrefix,
     llm_config=llm_config,
 )
 
 nurse = autogen.AssistantAgent(
     name="Nurse",
-    system_message="A nurse that can provide basic medical care and assist doctors.",
+    system_message="You are a nurse in an outpatient ward who provide compassionate and accurate information on post-treatment care, medication instructions, and recovery processes. Offer guidance for lifestyle modifications, preventive care and healthy lifestyle choices." + agentSystemPromptPrefix,
     llm_config=llm_config,
 )
 
 radiologist = autogen.AssistantAgent(
     name="Radiologist",
-    system_message="A radiologist that can interpret medical images and perform imaging tests.",
+    system_message="You are a Radiologist at a hospital, specialized in interpreting medical images such as X-rays, CT scans, MRIs, and ultrasounds to diagnose and aid in the treatment of patients." + agentSystemPromptPrefix,
     llm_config=llm_config,
 )
 
 surgeon = autogen.AssistantAgent(
     name="Surgeon",
-    system_message="A surgeon that can perform surgical procedures.",
+    system_message="You are a surgeon working at a hospital, specializing in complex surgical procedures, with a focus on minimally invasive techniques and patient rehabilitation." + agentSystemPromptPrefix,
     llm_config=llm_config,
 )
 
 gastroenterologist = autogen.AssistantAgent(
     name="Gastroenterologist",
-    system_message="A gastroenterologist that can diagnose and treat gastrointestinal disorders.",
+    system_message="You are a Gastroenterologist at a hospital, specialized in diagnosing and treating digestive system disorders, including those affecting the stomach, intestines, esophagus, liver, pancreas, and gallbladder." + agentSystemPromptPrefix,
     llm_config=llm_config,
 )
 
-groupchat = autogen.GroupChat(agents=[generalDoctor, nurse, radiologist, surgeon, gastroenterologist], messages=[], max_round=10)
+groupchat = autogen.GroupChat(agents=[patient, generalDoctor, nurse, radiologist, surgeon, gastroenterologist], messages=[], max_round=10)
 
 manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
 
-user_proxy.initiate_chat(
-    manager, message="``I have a huge pain in the right lower side of my tummy. Please help``"
+chat_result = patient.initiate_chat(
+    manager, message="I have a huge pain in the right lower side of my tummy. Please help"
 )
 
-with open("autogen_chat_messages.txt", "w") as f:
-    for message in user_proxy.chat_messages:
-        if isinstance(message, dict) and "agent" in message and "message" in message:
-            f.write(str(message["agent"]) + ": " + message["message"] + "\n")
+
+finalChat = []
+for message in chat_result.chat_history:
+    if message['role'] == "assistant":
+        if message['content'] != "":
+            finalChat.append(
+                {
+                    "role": "Patient",
+                    "content": message['content'],
+                }
+            )
+    else:
+        print(message)
+        finalChat.append(
+            {
+                "role": message['name'],
+                "content": message['content'],
+            }
+        )
+with open("autogen-responses.json", "w") as f:
+    f.write(json.dumps(finalChat))
